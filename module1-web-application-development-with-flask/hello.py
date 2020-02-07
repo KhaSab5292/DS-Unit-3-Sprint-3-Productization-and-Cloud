6fffde4ef6f988cd208eda572e3b03a8aba704a1
KhaSab5292 <KhaSab5292@users.noreply.github.com>
2020-02-07 18:12:55 -0500

    adding routes to hello.py

--- module1-web-application-development-with-flask/hello.py
+++ module1-web-application-development-with-flask/hello.py
@@ -1,6 +1,6 @@
 """Minimal flask app"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 #make the application
 app = Flask(__name__)
@@ -11,3 +11,10 @@ app = Flask(__name__)
 #Now define a function
 def hello():
     return "Hello World!"
+
+#Make a second route
+@app.route("/about")
+
+#Now make a the function that goes with about
+def preds():
+    return render_template('about.html')
